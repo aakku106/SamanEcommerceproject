1458da0b80d72a8051605d6f4f23a92818fa2df2
Adarasha Gaihre <adarashagaihre80@nepathyacollege.edu.np>
2024-06-21 21:09:08 +0545

    Fix broken navigation and return links in HTML files

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const close = document.querySelector(".close");
 const open = document.querySelector(".ham");
 const menu = document.querySelector(".menu");
+
 close.addEventListener("click", () => {
   menu.style.visibility = "hidden";
+  menu.style.opacity = "0"; // Optional for a smoother transition
 });
+
 open.addEventListener("click", () => {
   menu.style.visibility = "visible";
+  menu.style.opacity = "1"; // Optional for a smoother transition
 });
