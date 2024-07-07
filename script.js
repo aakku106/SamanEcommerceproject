b19d90221cacc05c31f9ab3619a7dbe3d1c56742
Adarasha Gaihre <adarashagaihre80@nepathyacollege.edu.np>
2024-07-07 22:59:47 +0545

    chore: Update CSS styles for headings and improve menu visibility transition

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const close = document.querySelector(".close");
 const open = document.querySelector(".ham");
 const menu = document.querySelector(".menu");
-const headings = document.querySelector(".headings");
 
 close.addEventListener("click", () => {
   menu.style.visibility = "hidden";
-  menu.style.opacity = "0"; 
+  menu.style.opacity = "0";
 });
 
 open.addEventListener("click", () => {
   menu.style.visibility = "visible";
-  menu.style.opacity = "1"; 
+  menu.style.opacity = "1";
 });
