ea34e455e134642eecdbce3fbbabec9cce1de5bd
Adarasha Gaihre <adarashagaihre80@nepathyacollege.edu.np>
2024-07-07 22:53:48 +0545

    chore: Update CSS styles for headings and add new class for headings in script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const close = document.querySelector(".close");
 const open = document.querySelector(".ham");
 const menu = document.querySelector(".menu");
+const headings = document.querySelector(".headings");
 
 close.addEventListener("click", () => {
   menu.style.visibility = "hidden";
